a2635ba5ea89617a44eb3838cf9a29bd669d8375
ivanzrobok <ivan.zrobok.itis.2020@lpnu.ua>
2020-10-11 23:04:47 +0300

    first repos_2

--- ConsoleApplication_lab2.2/ConsoleApplication_lab2.2.cpp
+++ ConsoleApplication_lab2.2/ConsoleApplication_lab2.2.cpp
@@ -12,16 +12,16 @@ int main()
 {
     double a;//вхідний параметр
     double b;//вхідний параметр
-    double z1; //результат обчислення 1 - го виразу
-    double z2;// результат обчислення 1-го виразу
+    //double z1; //результат обчислення 1 - го виразу
+    double z2;//результат обчислення 2 - го виразу
 
     cout << "a= "; cin >> a;
     cout << "b= "; cin >> b;
-    z1 = (cos(a) - cos(b)) * (cos(a) - cos(b)) - (sin(a) - sin(b)) * (sin(a) - sin(b));
+    //z1 = (cos(a) - cos(b)) * (cos(a) - cos(b)) - (sin(a) - sin(b)) * (sin(a) - sin(b));
     z2 = -4 * sin((a - b) / 2) * sin((a - b) / 2) * cos(a + b);
 
     cout << endl;
-    cout << "z1 =" << z1 << endl;
+    //cout << "z1 =" << z1 << endl;
     cout << "z2 =" << z2 << endl;
 
 
